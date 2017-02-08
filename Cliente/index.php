8549c08742323a0c65e7acd01786b6aa561f7e63
Josseline Arreola <j_uliane20@hotmail.com>
2017-02-07 20:18:49 -0600

    *

---
+++ Cliente/index.php
@@ -0,0 +1,37 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
+	 <link rel="stylesheet" type="text/css" href="css/style.css"> 
+</head>
+<body class="dark cyan" id="login">
+	<div id="login-page" class="row">
+    <div class="col s12 z-depth-6 card-panel">
+      <form class="login-form">
+        <div class="row">
+          <div class="input-field col s12 center">
+            <img src="img/logo.png" alt="" class="responsive-img valign profile-image-login">
+          </div>
+        </div>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-social-person-outline prefix"></i>
+            <input class="validate" id="text" type="text">
+            <label for="usuario" data-error="wrong" data-success="right" class="center-align">Usuario</label>
+          </div>
+        </div>
+     <div class="row">
+          <div class="input-field col s12">
+            <a href="index.php" class="btn waves-effect waves-light col s12">Login</a>
+          </div>
+        </div>
+      
+      </form>
+    </div>
+  </div>
+</body>
+</html>
+
