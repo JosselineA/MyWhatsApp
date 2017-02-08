4c338e5a9f5da2bf5184945afd7de311a6afdb87
Jesus <jesusguillermomendez@gmail.com>
2017-02-08 12:40:51 -0600

    Listado
    //Listado

--- Cliente/inicio.php
+++ Cliente/inicio.php
@@ -4,6 +4,7 @@
 	<title>MyWhatsApp</title>
 	<link rel="stylesheet" type="text/css" href="css/estilo.css">
 	<link rel="stylesheet" href="materialize/css/materialize.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	
 </head>
 <body>
@@ -23,8 +24,20 @@
 	</header>
 
 	<div class="row">
-		
-		<div id="contactos" class="col s3 teal accent-4">Chats</div>
+		<div id="Contacts" class="col s3 green lighten-4">
+ 		<div class="collection green darken-1">
+        <a href="#!" class="collection-item green lighten-5">
+        <p>Ernesto</p>   
+        </a>
+        <a href="#!" class="collection-item green lighten-5">
+        <h6>Javier</h6>
+        </a>
+        <a href="#!" class="collection-item green lighten-5">
+        <h6>Edgardo</h6>
+        </a>
+      	</div>
+
+		</div>
 		<div class="col s9 cyan accent-3">egg</div>
 
 	</div>
